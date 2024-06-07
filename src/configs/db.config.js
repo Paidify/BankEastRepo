0b4cbf77977f90d279f3fbe3bd860000f11b961b
Leonardo Vergara Márquez <leonardo11vergara11@gmail.com>
2024-06-07 03:01:22 -0500

    fix mongoose connection message

--- src/configs/db.config.js
+++ src/configs/db.config.js
@@ -1,10 +1,17 @@
 import mongoose from "mongoose";
 
-export const connect = async () => {
-  try {
-    mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to MongoDB");
-  } catch (err) {
-    console.error(err);
-  }
+export const connect = () => {
+  mongoose.connect(process.env.MONGODB_URI);
+
+  mongoose.connection.on("connected", function () {
+    console.log("Connected to Bank Database!");
+  });
+
+  mongoose.connection.on("error", function (err) {
+    console.log("Cannot connect to Bank Database", err);
+  });
+
+  mongoose.connection.on("disconnected", function () {
+    console.log("Disconnected from Bank Database");
+  });
 };
